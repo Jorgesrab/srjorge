d338097c466dce978b16b258f0ed4ee86d2cbb25
Jorgesrab <jorge.sanchez174@educa.madrid.org>
2024-05-09 13:17:51 +0200

    Cambio

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Este es el cambio desde git");
+        System.out.println("Este es el cambio del punto 8 desde intelij");
     }
 }
