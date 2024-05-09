733ea476b1db148f66c9a976d9725d8a4964a028
Jorgesrab <146349955+Jorgesrab@users.noreply.github.com>
2024-05-09 13:08:31 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Este es el cambio del punyo 7 git");
+        System.out.println("Este es el cambio del punyo 8 git");
     }
 }
